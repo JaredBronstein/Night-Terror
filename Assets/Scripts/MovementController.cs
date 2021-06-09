9c9480d53f2bfaca18fa9c22d4f00da05fde7b52
Jared Bronstein <43156735+JaredBronstein@users.noreply.github.com>
2021-06-09 01:28:39 -0500

    Implemented Movement System

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    [SerializeField]
+    private Room currentRoom;
+
+    [SerializeField]
+    private GameObject camera;
+
+    private Room[] adjacentRooms;
+
+    private void Awake()
+    {
+        SetAdjacent();
+    }
+
+    private void SetAdjacent()
+    {
+        adjacentRooms = currentRoom.getAdjacent();
+    }
+
+    public void Move(int i)
+    {
+        if(adjacentRooms[i] != null)
+        {
+            currentRoom = adjacentRooms[i];
+            SetAdjacent();
+            camera.transform.position = currentRoom.getCameraPosition().position;
+        }
+        else
+        {
+            Debug.Log("There is no room in this direction");
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("Left"))
+        {
+            Move(0);
+        }
+        else if (Input.GetButtonDown("Right"))
+        {
+            Move(1);
+        }
+        else if (Input.GetButtonDown("Up"))
+        {
+            Move(2);
+        }
+        else if (Input.GetButtonDown("Down"))
+        {
+            Move(3);
+        }
+    }
+}
---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    [SerializeField]
+    private Transform cameraPosition;
+
+    [Tooltip("Group of Adjacent Rooms, goes Left, Right, Forward, Back")]
+    [SerializeField]
+    private Room[] adjacentRooms = new Room[4];
+
+    public Transform getCameraPosition()
+    {
+        return cameraPosition;
+    }
+
+    public Room[] getAdjacent()
+    {
+        return adjacentRooms;
+    }
+}
